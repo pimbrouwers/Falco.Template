a8dd9a004795cae5422d7e4c8b29b2c304505df8
Pim Brouwers <pimnation@gmail.com>
2024-12-17 09:36:41 -0500

    5.0.0

--- src/Falco.Template/Basic/Program.fs
+++ src/Falco.Template/Basic/Program.fs
@@ -1,16 +1,34 @@
-module AppName.Program
+namespace AppName
 
-open Falco
-open Microsoft.AspNetCore.Builder 
+module App =
+    open Falco
+    open Falco.Routing
 
-[<EntryPoint>]
-let main args =
-    let bldr = WebApplication.CreateBuilder(args)
-    let wapp = bldr.Build()
-    
-    wapp.Use(DeveloperExceptionPageExtensions.UseDeveloperExceptionPage)
-        .UseFalco()
-        .FalcoGet("/", Response.ofPlainText "Hello World")
-        .FalcoNotFound(Response.withStatusCode 404 >> Response.ofEmpty)
-        .Run()
-    0
+    let endpoints =
+        [
+            get "/" (Response.ofPlainText "Hello World!")
+        ]
+
+module Program =
+    open Falco
+    open Microsoft.AspNetCore.Builder
+    open Microsoft.Extensions.DependencyInjection
+
+    [<EntryPoint>]
+    let main args =
+        let bldr = WebApplication.CreateBuilder(args)
+
+        // register services
+        bldr.Services
+            .AddRouting()
+            |> ignore
+
+        let wapp = bldr.Build()
+
+        // assemble middleware pipeline
+        wapp.Use(DeveloperExceptionPageExtensions.UseDeveloperExceptionPage)
+            .UseRouting()
+            .UseFalco(App.endpoints)
+            .UseFalcoNotFound(Response.withStatusCode 404 >> Response.ofPlainText "Not Found")
+            .Run()
+        0
